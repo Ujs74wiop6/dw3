d4c4b14e08dc18746fa13af906125bc3b1ed3bb4
Ujs74wiop6 <fabricioujs74@gmail.com>
2024-09-09 16:35:48 -0300

    update: 09/09/2024

--- 1_primeiro/app.js
+++ 1_primeiro/app.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.send('Hello DW3!')
 })
 
+app.get('/dw3', (req, res) => {
+    res.send('Segunda Rota')
+})
+
 app.listen(port, () => {
     console.log("Executando a aplicação ", process.env.APP_NAME);
     console.log("Example app listening on port ", process.env.PORT);
